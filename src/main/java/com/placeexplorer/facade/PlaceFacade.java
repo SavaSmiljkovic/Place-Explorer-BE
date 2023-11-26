b2333fb7fdd29b5f89a964af0536638eda344452
SavaSmiljkovic <savasmiljkov@gmail.com>
2023-11-26 17:44:10 +0100

    CDT-10: Facade Implementation

---
+++ src/main/java/com/placeexplorer/facade/PlaceFacade.java
@@ -0,0 +1,36 @@
+package com.placeexplorer.facade;
+
+import com.placeexplorer.model.dto.PlaceFrontendDTO;
+import com.placeexplorer.model.dto.PlaceOriginalDTO;
+
+public interface PlaceFacade {
+
+    /**
+     * Converts PlaceOriginalDTO to PlaceFrontendDTO without querying database.
+     *
+     * @param placeOriginalDTO DTO that will be converted to UI DTO form
+     */
+    PlaceFrontendDTO get(PlaceOriginalDTO placeOriginalDTO);
+
+    /**
+     * Gets Place from database by uid.
+     *
+     * @param uid UID of requested Place
+     */
+    PlaceFrontendDTO getFromDB(String uid);
+
+    /**
+     * Saves Place in database in background asynchronously.
+     *
+     * @param placeOriginalDTO DTO that will be converted and saved in database
+     */
+    void saveAsynchronously(PlaceOriginalDTO placeOriginalDTO);
+
+    /**
+     * Checks if Place exists in database by uid.
+     *
+     * @param uid UID of requested Place
+     */
+    boolean existsByUid(String uid);
+
+}
