38819bc6f79a15c8d1cdf40e142fa3d46ea8a03d
SavaSmiljkovic <savasmiljkov@gmail.com>
2023-11-26 17:50:41 +0100

    CDT-23: Dedicated exception for place converters

---
+++ src/main/java/com/placeexplorer/errorHandler/dedicatedException/PlaceConverterException.java
@@ -0,0 +1,9 @@
+package com.placeexplorer.errorHandler.dedicatedException;
+
+public class PlaceConverterException extends RuntimeException {
+
+    public PlaceConverterException(String message) {
+        super(message);
+    }
+
+}
