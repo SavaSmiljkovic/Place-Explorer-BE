049be5831af067a5aa156de8396490d64ee122f7
SavaSmiljkovic <savasmiljkov@gmail.com>
2023-11-30 19:10:32 +0100

    CDT-32: cors configuration

--- src/main/java/com/placeexplorer/security/WebSecurity.java
+++ src/main/java/com/placeexplorer/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.placeexplorer.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,8 +25,9 @@ public class WebSecurity {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.requestMatchers(openPaths.split(",")).permitAll().anyRequest().authenticated());
-        return http.build();
+        return http.authorizeHttpRequests(requests -> requests.requestMatchers(openPaths.split(",")).permitAll().anyRequest().authenticated())
+                   .cors(Customizer.withDefaults())
+                   .build();
     }
 
     @Bean
